fa36c66ddce6c4cb864f5cde1ec442b5e6ab44b0
Debo06 <choudhud@csp.edu>
2025-08-01 19:38:15 -0700

    Update process.py
    update the comments

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 def clean_and_process(df):
-    df = df.drop_duplicates(subset=['SKU', 'Location'])
+    df = df.drop_duplicates(subset=['SKU', 'Location']) # data processed uysing Pandas Functions
     df['OnHandQty'] = df['OnHandQty'].apply(lambda x: max(x, 0))
     df['ReorderQty'] = np.maximum(0, df['ReorderPoint'] - df['OnHandQty'])
     df['NeedsRestock'] = df['ReorderQty'] > 0
