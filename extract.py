9c4cdd4406b57af148b4c4dcaa9a46842a837476
Debo06 <choudhud@csp.edu>
2025-08-01 19:38:57 -0700

    Update extract.py
    Updated the file path

--- extract.py
+++ extract.py
@@ -3,9 +3,9 @@
 
 import pandas as pd
 
-def load_inventory_data(filepath):
+def load_inventory_data("data/inventory_raw.csv):
     try:
-        df = pd.read_csv(filepath)
+        df = pd.read_csv("data/inventory_raw.csv)
         return df
     except Exception as e:
         raise RuntimeError(f"Error loading inventory data: {e}")
